9239ca9ad2f7aa543011385b39326f3ef1a67240
Suryakantray <surya.25b01010352@abes.ac.in>
2026-08-03 10:05:37 +0530

    next lab modified

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(null==undefined)
+console.log(Number.MAX_SAFE_INTEGER)
+const a=10n
+console.log(typeof(a))
+
+const x=9007199254740991n
+const y=10n
+
+console.log(x+y)
+console.log(x*y)
+console.log(x-y)
+console.log(x/y)
