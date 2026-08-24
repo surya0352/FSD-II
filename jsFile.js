dac3f6aa6fea87c7dc3874a0db66485781e042c4
Suryakantray <surya.25b01010352@abes.ac.in>
2026-08-24 09:37:13 +0530

    string

---
+++ jsFile.js
@@ -0,0 +1,55 @@
+// let z=10;
+// let s=" ";
+// console.log(typeof s);
+// let str1=String('10');
+// console.log(s+=str1)
+
+
+// //Multiline String
+// let str=` this is 
+// multiline string
+// in javascript`;
+// console.log(str);
+
+// //Accessing Characters
+// let x='JavaScript';
+// x[0]='A';
+// console.log(x);
+
+// const y='javaScript'
+// y[0]='A';
+// console.log(y)
+
+
+// //object
+// const str5='hello';
+// const str6= new String("hello")
+// console.log(str5==str6)
+// console.log(typeof str5)
+// console.log(str5===str6)
+// console.log(typeof str6)
+
+
+// //slice(start,end)
+
+// let str2="surya";
+// console.log(str2)
+// let str3='A'+str2.slice(1);
+// console.log(str3);
+// console.log(str2)
+
+
+// const fruits=["apple","banana","mango","grapes"]
+// const result=fruits.slice(1,3)
+// console.log(fruits)
+// console.log(result)
+
+
+// const fruits=["apple","banana","mango","grapes"]
+// const copy=fruits.slice()
+// console.log(fruits)
+// console.log(copy)
+// console.log(copy.slice(-1))
+// console.log(copy.slice(-2))
+
+let stri="surya"
