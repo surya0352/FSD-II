0a221acc0c85376c063ef878b88dfb907bb8248e
Suryakantray <surya.25b01010352@abes.ac.in>
2026-09-17 10:01:57 +0530

    calulator using arror function   higher order funciton

--- calculator.js
+++ calculator.js
@@ -25,3 +25,18 @@
 // console.log("Division:", calculator(10, 5, divide));
 
 
+// //calulator using arror function   higher order funciton
+
+// const calculator = (a, b, operation) => {
+//     return operation(a, b);
+// };
+
+// const add = (a, b) => a + b;
+// const subtract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+// const divide = (a, b) => a / b;
+
+// console.log("Addition:", calculator(10, 5, add));
+// console.log("Subtraction:", calculator(10, 5, subtract));
+// console.log("Multiplication:", calculator(10, 5, multiply));
+// console.log("Division:", calculator(10, 5, divide));
