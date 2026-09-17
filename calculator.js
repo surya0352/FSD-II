5ba7605abe1ce4561f6189cf6de364fcd3f5f3ee
Suryakantray <surya.25b01010352@abes.ac.in>
2026-09-17 10:01:26 +0530

    calculator in js using callback

---
+++ calculator.js
@@ -0,0 +1,27 @@
+
+// // calculator in js using callback
+// function calculator(a, b, operation) {
+//     return operation(a, b);
+// }
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// function divide(a, b) {
+//     return a / b;
+// }
+// console.log("Addition:", calculator(10, 5, add));
+// console.log("Subtraction:", calculator(10, 5, subtract));
+// console.log("Multiplication:", calculator(10, 5, multiply));
+// console.log("Division:", calculator(10, 5, divide));
+
+
